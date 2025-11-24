9fa233e58aaf202757c5db3a522c5de90e934880
FOUAD SAIDI <fouadsai17@gmail.com>
2025-11-24 10:33:41 +0100

    v1.1

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import Index from "./pages/Index";
 import About from "./pages/About";
 import Products from "./pages/Products";
@@ -13,21 +13,38 @@ import NotFound from "./pages/NotFound";
 const queryClient = new QueryClient();
 
 const App = () => (
+  // <QueryClientProvider client={queryClient}>
+  //   <TooltipProvider>
+  //     <Toaster />
+  //     <Sonner />
+  //     <BrowserRouter>
+  //       <Routes>
+  //         <Route path="/" element={<Index />} />
+  //         <Route path="/about" element={<About />} />
+  //         <Route path="/products" element={<Products />} />
+  //         <Route path="/quality" element={<Quality />} />
+  //         <Route path="/contact" element={<Contact />} />
+  //         {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+  //         <Route path="*" element={<NotFound />} />
+  //       </Routes>
+  //     </BrowserRouter>
+  //   </TooltipProvider>
+  // </QueryClientProvider>
+
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter> {/* <-- ici */}
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
           <Route path="/products" element={<Products />} />
           <Route path="/quality" element={<Quality />} />
           <Route path="/contact" element={<Contact />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
